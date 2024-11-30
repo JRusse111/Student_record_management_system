12d0bccd1a02b99431f608bf654df74fdbcb544e
Aekard <lorddrakebaza@gmail.com>
2024-11-30 16:15:56 +0800

    Test

---
+++ src/main/java/com/src/Student_record_management_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.src;
+
+/**
+ *
+ * @author LordD
+ */
+public class Student_record_management_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
